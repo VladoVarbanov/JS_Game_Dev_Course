7906155bd75b10c8fe67ef823b2da4c4dfdc3109
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-03-28 18:43:18 +0200

    Create Explosion class

---
+++ Project 04/Classes/Explosion.js
@@ -0,0 +1,44 @@
+export class Explosions {
+  constructor(x, y) {
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth * 0.7;
+    this.height = this.spriteHeight * 0.7;
+    this.x = x;
+    this.y = y;
+    this.image = new Image();
+    this.image.src = "../Pictures/boom.png";
+    this.frame = 0;
+    this.timer = 0;
+    this.angle = Math.random() * 6.2;
+    this.sound = new Audio();
+    this.sound.src = "../Audio/Ice attack 2.wav";
+  }
+  /** Methods */
+  update() {
+    if (this.frame === 0) {
+      this.sound.play();
+    }
+    this.timer++;
+    if (this.timer % 10 === 0) {
+      this.frame++;
+    }
+  }
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.drawImage(
+      this.image,
+      this.spriteWidth * this.frame,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      0 - this.width / 2,
+      0 - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.restore();
+  }
+}
